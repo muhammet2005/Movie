9ce9a286705b696e4881b4724fe1cb0af8c75a7d
muhammet2005 <maxmudgamer009@gmail.com>
2023-11-02 20:27:23 +0700

    first commit

--- src/pages/Now_playing/index.js
+++ src/pages/Now_playing/index.js
@@ -4,12 +4,12 @@ import { key } from '../../API_KEY'
 import MovieCard from '../MovieCard';
 
 const Now_playing = () => {
-    const [now_playing,setNowplaying]=useState([])
+    const [nowPlaying,setNowPlaying]=useState([])
     const getNowPlaying = () => {
         axios(`https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=3`)
         .then(res=>{
             console.log(res);
-            setNowplaying(res.data.results)
+            setNowPlaying(res.data.results)
         })
     }
     useEffect(()=>{
@@ -21,7 +21,7 @@ const Now_playing = () => {
                 <div className='popular'>
                     <h1>Now playing</h1>
                     <div className='popular-movie'>
-                    {now_playing.map((el)=> <MovieCard elem={el}/>) }
+                    {nowPlaying.map((el)=> <MovieCard elem={el}/>) }
                   </div>
                 </div>
             </div>
