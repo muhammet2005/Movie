8f3d44fe03bfba15f924c6dc2f6a8618d8be4efe
muhammet2005 <maxmudgamer009@gmail.com>
2023-11-02 20:32:25 +0700

    first commit

---
+++ src/pages/UpComing/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UpComing = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="up_coming">
+                    <div className="movieInfo">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default UpComing;
